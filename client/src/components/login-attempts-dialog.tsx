cf25c749d47fd49bdce45fed423af2884036a61a
tommyshorez <27781699-tommyshorez@users.noreply.replit.com>
2025-02-26 20:52:41 +0000

    Fix: Import React in login-attempts-dialog.tsx
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/4106dc7c-280d-4d11-8b07-5a9e2a9d55f4/ca59a083-cb09-47f0-9d5b-328c606df2fa.jpg

--- client/src/components/login-attempts-dialog.tsx
+++ client/src/components/login-attempts-dialog.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { useQuery } from "@tanstack/react-query";
